30d69afd96cb8323b080146bd3c31eb07019581e
Orleandro <76212175+Vornak@users.noreply.github.com>
2023-02-23 10:14:43 -0300

    atualização de métodos

--- primeirocrud/src/main/java/com/angularcrud/primeirocrud/model/PessoaModel.java
+++ primeirocrud/src/main/java/com/angularcrud/primeirocrud/model/PessoaModel.java
@@ -22,7 +22,54 @@ public class PessoaModel {
     @Column(name = "sobrenome", length = 150, nullable = true)
     private String sobrenome;
 
+    @Column(name = "idade", length = 3, nullable = true)
+    private String idade;
 
+    @Column(name = "nascimento", length = 50, nullable = true)
+    private String nascimento;
+
+   @Column(name = "naturalidade", length = 100, nullable = true)
+    private String naturalidade;
+
+
+
+
+    public String getNaturalidade() {
+    return naturalidade;
+}
+
+public void setNaturalidade(String naturalidade) {
+    this.naturalidade = naturalidade;
+}
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    @Column(name = "genero", length = 16, nullable = true)
+    private String genero;
+
+
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
 
     public Long getId() {
         return this.id;
